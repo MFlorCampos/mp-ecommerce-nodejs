4cf614a06d273f1673b656b38b7166159df3642e
Flor Campos <m.florcampos99@gmail.com>
2020-10-01 13:12:10 -0300

    checkout integration

--- app.js
+++ app.js
@@ -19,3 +19,26 @@ app.use(express.static('assets'));
 app.use('/assets', express.static(__dirname + '/assets'));
  
 app.listen(process.env.PORT || 3000);
+  
+const mercadopago = require ('mercadopago');
+
+mercadopago.configure({
+  access_token: 'TEST-6511441310885867-100114-9c8c7cb674ad2a36d54d6452810ea089-346810035'
+});
+
+let preference = {
+    items: [
+      {
+        title: 'Mi producto',
+        unit_price: 100,
+        quantity: 1,
+      }
+    ]
+  };
+  
+  mercadopago.preferences.create(preference)
+  .then(function(response){
+    global.id = response.body.id;
+  }).catch(function(error){
+    console.log(error);
+  });
